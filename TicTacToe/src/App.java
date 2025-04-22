b522670d9084be9c0e05bf1cd7926ac176b06f56
0Eyitayo0 <eyitayo651@gmail.com>
2025-04-21 19:51:57 -0500

    Update App.java

--- TicTacToe/src/App.java
+++ TicTacToe/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         TicTacToe ticTacToe = new TicTacToe();
+        System.out.println("Hello World. Game has ended and Game Board uas closed!")
     }
 }
  
